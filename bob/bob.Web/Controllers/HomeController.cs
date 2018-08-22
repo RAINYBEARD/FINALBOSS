64eeabf6d7f49c6157b11afdcf1be289f75fd2c0
Marcelo Rosende <Crusader14579@hotmail.com>
2018-08-22 20:58:52 -0300

    Fixes to Database Config

--- bob/bob.Web/Controllers/HomeController.cs
+++ bob/bob.Web/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using bob.Data;
+
 
 namespace bob.Controllers
 {
@@ -10,7 +12,14 @@ namespace bob.Controllers
     {
         public ActionResult Index()
         {
+            CaeceDbContext context = new CaeceDbContext();
+
             ViewBag.Title = "Home Page";
+            var materia = context.materias.Create();
+            materia.materiaid = 8015;
+            materia.abr = "INTRO. A LA INFORMATICA";
+            context.materias.Add(materia);
+            context.SaveChanges();
 
             return View();
         }
