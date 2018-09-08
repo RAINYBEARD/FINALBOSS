b80d03723ac553502024d2360a745724fe712ba5
Coronel Kohler <nachodemontmollin@gmail.com>
2018-09-07 23:36:16 -0300

    rellenando el resto de los diccionarios

--- bob/bob.Web/Controllers/HomeController.cs
+++ bob/bob.Web/Controllers/HomeController.cs
@@ -92,24 +92,94 @@ namespace bob.Controllers
         //public ActionResult Nacho()
         //{
         //    var dict = new AprDictionary();
+        //    var dict = new CurDictionary();
+        //    var dict = new EquivDictionary();
+        //    var dict = new NotCurDictionary();
+        //    var dict = new MesaFinalDictionary();
+        //    var dict = new CursosDictionary();
         //    //dict.Add()
         //    foreach (HistoriaAcademica dato in LoadJson<HistoriaAcademica>(MockMethod.HistoriaAcademica))
         //    {
         //        char estado_materia = char.Parse(dato.descrip);
-        //        if (estado_materia == 'A')
+        //        if (estado_materia == 'APR')
         //        {
-           
-        //            string mid = dato.materia_id;
-        //            string fec = dato.fecha;
-        //            string ab = dato.abr;
-        //            int not = dato.nota;
-        //            string prof = dato.profesor;
-        //            string act = dato.acta_id;
-        //            int an = dato.anio;
-        //            int cua = dato.cuatrim;
-        //            dict.Add(mid, fec, ab, not, prof, act, an, cua);
+
+        //            AprValue apr;
+        //            string materia_id = dato.materia_id;
+        //            apr.Fecha = dato.fecha;
+        //            apr.Calificacion = dato.calificacion;
+        //            apr.Descripcion = dato.descrip;
+        //            apr.Profesor = dato.profesor;
+        //            apr.Acta_id = dato.acta_id;
+        //            apr.Anio = dato.anio;
+        //            apr.Cuatrim = dato.cuatrim;
+        //            dict.Add(materia_id, apr);
         //        }
+        //        else {
+        //               if (estado_materia == 'CUR') 
+        //               {
+        //                 CurValue cur;
+        //                 cur.Fecha = fecha;
+        //                 cur.Abr = abr;
+        //                 cur.Profesor = profesor;
+        //                 cur.Anio = anio;
+        //                 cur.Cuatrim = cuatrim;
+        //                 cur.Plan_id = plan_id;
+        //                 cur.Plan_tit = plan_tit;
+        //                 this.Add(materia_id, cur);
+        //               }
+        //               else 
+        //               {
+        //                  if (estado_materia == 'PEN') 
+        //                  {
+        //                    EquivVale equiv;
+        //                    cur.Fecha = fecha;
+        //                    cur.Abr = abr;     
+        //                    cur.Plan_id = plan_id;
+        //                    cur.Plan_tit = plan_tit;
+        //                    this.Add(materia_id, equiv);
+        //                  }
+        //                  else 
+        //                  {
+        //                    NotCurValue notcur;
+        //                    notcur.Abr = abr;
+        //                    notcur.Profesor = profesor;
+        //                    notcur.Anio = anio;
+        //                    notcur.Cuatrim = cuatrim;
+        //                    notcur.Plan_id = plan_id;
+        //                    notcur.Plan_tit = plan_tit;
+        //                    this.Add(materia_id, notcur);
+        //                  }
+        //               }
+        //             }
         //    }
+        //    foreach (MesaFinal dato in LoadJson<MesaFinal>(MockMethod.MesasFinal))
+        //     { 
+        //       MesaFinalValue  mesafinal;
+        //       string materia_id = dato.materia_id;
+        //       mesafinal.Turno_id = dato.Turno_id;
+        //       mesafinal.Plan_id = dato.Plan_id
+        //       mesafinal.Profesor = dato.Profesor;
+        //       mesafinal.Sede_id = dato.Sede_id;
+        //       dict.Add(materia_id, mesafinal);
+        //     }
+        //     foreach (Cursos dato in LoadJson<Cursos>(MockMethod.Cursos))
+        //     {
+        //       char turno_materia = char.Parse(dato.turno_id);
+        //       string dia_materia = dato.dia;
+        //       if (turno_materia == 'N') || ((dia_materia == '0000010') || (dia_materia == '0000020') !! (dia_materia == '0000030')) || (dato.materia_id = '2091')
+        //       {
+        //          CursosValue cursos;
+        //          string materia_id = dato.materia_id;
+        //          cursos.Turno_id = dato.turno_id;
+        //          cursos.Dia = dato.dia;
+        //          cursos.M_acobrar = dato.m_acobrar;
+        //          cursos.Plan_id = dato.plan_id;
+        //          dict.Add(materia_id, cursos);
+        //       }
+        //       
+        //         
+        //     }
         //    return View("Index.cshtml");
         //}
 
@@ -186,33 +256,93 @@ public class HistoriaAcademica
     public string materia_id;
     public string fecha;
     public string abr;
-    public int nota;
+    public int calificacion;
     public string profesor;
     public string acta_id;
     public int anio;
     public int cuatrim;
 }
 
+public class MesaFinal
+{
+    public string materia_id;
+    string plan_id;
+    char turno_id;
+    string profesor;
+    char sede_id;
+}
+
+public class Cursos
+{
+    public string materia_id;
+    string plan_id;
+    char turno_id;
+    string dia;
+    int m_acobrar;
+
+
+}
+
 //struct AprValue {
 //    string Fecha  
-//    Abr = ab
-//    Nota = n
-//    Profesor
-//    Acta_id
-//    Anio = a
-//    Cuatrim
+//    string Abr
+//    int Calificacion
+//    string Profesor
+//    string Acta_id
+//    int Anio 
+//    int Cuatrim
+//}
+
+//struct CurValue {
+//    string Fecha  
+//    string Abr
+//    string Profesor
+//    int Anio 
+//    int Cuatrim
+//    string Plan_id
+//    int Plan_tit
+//}
+
+//struct EquivValue {
+//    string Fecha  
+//    string Abr
+//    string Plan_id
+//    int Plan_tit
+//}
+
+//struct NotCurValue {
+//    string Fecha  
+//    string Abr
+//    int Anio 
+//    int Cuatrim
+//    string Plan_id
+//    int Plan_tit
+//}
+
+//struct MesaFinalValue {
+//    char Turno_id 
+//    string Plan_id
+//    string Profesor 
+//    char sede_id 
+//}
+
+//struct CursosValue {
+//    string Plan_id
+//    char Turno_id
+//    string Dia 
+//    int M_acobrar
 //}
 
 //public class AprDictionary : Dictionary<string, AprValue>
 //{
 
-//    public void Add(string materia_id, string fecha, string abr, int nota, string profesor, string acta_id, int anio, int cuatrim)
+//    public void Add(string materia_id, string fecha, string abr, int calificacion, string profesor, string acta_id, int anio, int cuatrim)
 //    {
 
 //        AprValue apr;
 //        apr.Fecha = fecha;
 //        apr.Abr = abr;
-//        apr.Nota = nota;
+//        apr.Calificacion = calificacion;
 //        apr.Profesor = profesor;
 //        apr.Acta_id = acta_id;
 //        apr.Anio = anio;
@@ -220,3 +350,75 @@ public class HistoriaAcademica
 //        this.Add(materia_id, apr);
 //    }
 //}
+//public class CurDictionary : Dictionary<string, CurValue>
+//{
+
+//    public void Add(string materia_id, string fecha, string abr, string profesor,  int anio, int cuatrim, string plan_id, int plan_tit)
+//    {
+//        CurValue cur;
+//        cur.Fecha = fecha;
+//        cur.Abr = abr;
+//        cur.Profesor = profesor;
+//        cur.Anio = anio;
+//        cur.Cuatrim = cuatrim;
+//        cur.Plan_id = plan_id;
+//        cur.Plan_tit = plan_tit;
+//        this.Add(materia_id, cur);
+//    }
+//}
+
+//public class EquivDictionary : Dictionary<string, EquivValue>
+//{
+
+//    public void Add(string materia_id, string fecha, string abr, string plan_id, int plan_tit)
+//    {
+//        EquivValue equiv;
+//        cur.Fecha = fecha;
+//        cur.Abr = abr;     
+//        cur.Plan_id = plan_id;
+//        cur.Plan_tit = plan_tit;
+//        this.Add(materia_id, equiv);
+//    }
+//}
+
+//public class NotCurDictionary : Dictionary<string, NotCurValue>
+//{
+
+//    public void Add(string materia_id, string abr, int anio, int cuatrim, string plan_id, int plan_tit)
+//    {
+//        NotCurValue notcur;
+//        notcur.Abr = abr;
+//        notcur.Profesor = profesor;
+//        notcur.Anio = anio;
+//        notcur.Cuatrim = cuatrim;
+//        notcur.Plan_id = plan_id;
+//        notcur.Plan_tit = plan_tit;
+//        this.Add(materia_id, notcur);
+//    }
+//}
+
+// public class MesaFinalDictionary : Dictionary<string, MesaFinalValue>
+//  {
+//     public void Add(string materia_id, char turno_id, string plan_id, string profesor, char sede_id)
+//   {
+//      MesaFinalValue mesafinal
+//      mesafinal.Turno_id = turno_id;
+//      mesafinal.Plan_id = plan_id;
+//      mesafinal.Profesor = profesor;
+//      mesafinal.Sede_id= sede_id;
+//      this.Add(materia_id, mesafinal);
+//   }
+//  }
+
+// public class CursosDictionary : Dictionary<string, Cursosvalue>
+// {
+//   public void Add(string materia_id, char turno_id, string dia, int m_acobrar, string plan_id, int plan_tit)
+//    {
+//      CursosValue cursos
+//      cursos.Turno_id = turno_id;
+//      cursos.Dia = dia;
+//      cursos.M_acobrar = m_acobrar;
+//      cursos.Plan_id = plan_id;
+//      this.Add(materia_id, cursos);
+//    }
+// }
