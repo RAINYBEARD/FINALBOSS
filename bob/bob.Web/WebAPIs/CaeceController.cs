eb5a77d93702ff03db3e0eeb9db331cfc0a57b77
chrandres <chrandres@gmail.com>
2018-09-28 17:46:16 -0300

    Agrego un parametro mas a prueba para testear la funcionalidad de una forma mas practica

--- bob/bob.Web/WebAPIs/CaeceController.cs
+++ bob/bob.Web/WebAPIs/CaeceController.cs
@@ -439,14 +439,14 @@ namespace bob.Controllers
         }
 
         /// <summary>
-        /// Ejemplo de llamada: http://localhost:52178/Caece/Prueba/?filtrodias=1000000
+        /// Ejemplo de llamada: http://localhost:52178/Caece/Prueba/?filtrodias=1000000&filtrocantdias=1
         /// </summary>
         /// <param name="matricula"></param>
         [HttpGet]
-        [Route("Prueba/{filtrodias}")]
-        public void Prueba(string filtrodias)
+        [Route("Prueba/{filtrodias}/{filtrocantdias}")]
+        public void Prueba(string filtrodias, int filtrocantdias)
         {
-            List<Curso> materias = MostrarMateriasACursarCuatrimestreActual("951282", filtrodias, 7, "auto");
+            List<Curso> materias = MostrarMateriasACursarCuatrimestreActual("951282", filtrodias, filtrocantdias, "auto");
             //List<Curso> materias = MostrarMateriasACursarCuatrimestreActual("951282", filtrodias, 1, "manual");
 
             foreach (var materia in materias)
