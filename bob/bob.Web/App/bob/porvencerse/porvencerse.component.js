cae030c35aae964fad4e7bacc48a2ff0fb9b81fd
Coronel Kohler <nachodemontmollin@gmail.com>
2019-01-09 20:01:39 -0300

    Tabla con finales por vencerse agregada

---
+++ bob/bob.Web/App/bob/porvencerse/porvencerse.component.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('bob').component('porvencerse', {
+        controllerAs: 'vm',
+        controller: function (caeceService) {
+            var vm = this;
+            vm.matricula;
+            vm.finales;
+            vm.submit = submit;
+
+            function submit() {
+
+                caeceService.getFinales(vm.matricula).then(function (response) {
+                    vm.finales = response;
+                });
+            }
+
+        },
+        templateUrl: '/App/bob/porvencerse/porvencerse.component.html'
+    });
+
+})();
